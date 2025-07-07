2ce30a1e9b493eb38f16a7ac8f75bdd77aca1abf
Ryanocerou5 <85260279+Ryanocerou5@users.noreply.github.com>
2025-07-07 14:48:30 +1000

    Implemented basic SFX manager

--- FiveGuys/Assets/Scripts/SFXManager.cs
+++ FiveGuys/Assets/Scripts/SFXManager.cs
@@ -1,4 +1,8 @@
+using System;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using Random = UnityEngine.Random;
 
 public class SFXManager : MonoBehaviour
 {
@@ -15,18 +19,48 @@ public class SFXManager : MonoBehaviour
     }
 
     // Plays SFX at point
-    public void playSFX(AudioClip audioClip, Transform spawnTransform)
+    public void PlaySFX(AudioClip audioClip, Transform spawnTransform, float volume)
     {
         // Spawn in gameobject
         AudioSource audioSource =  Instantiate(sfxObject, spawnTransform.position, Quaternion.identity);
-        // Assign audio source component
 
         // Assign audio clip
+        audioSource.clip = audioClip;
+
+        // Assign audio volume
+        audioSource.volume = volume;
 
         // Play audio clip
+        audioSource.Play();
 
         // Get length of audio clip
+        float clipLength = audioSource.clip.length;
 
         // Destroy object after time elapsed
+        Destroy(audioSource.gameObject, clipLength);
+    }
+
+    // Play random SFX from array
+    public void PlayRandomSFX(AudioClip[] audioClip, Transform spawnTransform, float volume)
+    {
+        int rand = Random.Range(0, audioClip.Length);
+
+        // Spawn in gameobject
+        AudioSource audioSource =  Instantiate(sfxObject, spawnTransform.position, Quaternion.identity);
+
+        // Assign audio clip
+        audioSource.clip = audioClip[rand];
+
+        // Assign audio volume
+        audioSource.volume = volume;
+
+        // Play audio clip
+        audioSource.Play();
+
+        // Get length of audio clip
+        float clipLength = audioSource.clip.length;
+
+        // Destroy object after time elapsed
+        Destroy(audioSource.gameObject, clipLength);
     }
 }
