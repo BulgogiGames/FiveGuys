cfdaaa7af29f88974e437d3930cb3cc237e5d897
EJ-Corp <79449797+EJ-Corp@users.noreply.github.com>
2025-07-07 12:58:05 +1000

    Added the 5 guys

--- FiveGuys/Assets/Scripts/CharacterNavigation.cs
+++ FiveGuys/Assets/Scripts/CharacterNavigation.cs
@@ -4,6 +4,8 @@ using UnityEngine.AI;
 public class CharacterNavigation : MonoBehaviour
 {
     [SerializeField] private Transform target;
+    [SerializeField] private Transform workingStation;
+
 
     private NavMeshAgent agent;
 
